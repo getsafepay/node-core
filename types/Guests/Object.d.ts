6d8fb607bb17c844f559298437cb2acc602e9ccd
fatimaaurangzeb0640 <fatimaaurangzeb0640@gmail.com>
2025-02-25 12:32:35 +0500

    adds guest v2 api

--- types/Guests/Object.d.ts
+++ types/Guests/Object.d.ts
@@ -3,6 +3,8 @@ declare module "@sfpy/node-core" {
     namespace Guests {
       class Object {
         create(params?: any, options?: any): Promise<any>;
+
+        createV2(params?: any, options?: any): Promise<any>;
       }
     }
   }
